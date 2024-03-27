35a03b7ee6f3ca7b33c91434449c773c90dced6b
Poulinakis_Konstantinos <poulinakis.kon@gmail.com>
2024-03-27 07:04:55 +0100

    lint

--- test_blocks.py
+++ test_blocks.py
@@ -1,6 +1,7 @@
 import numpy as np
 from blocks import Identity, Relu, Neuron, NaiveClassifier
 
+
 def test_identity():
     identity = Identity()
     input_data = np.array([1, 2, 3])
@@ -8,11 +9,13 @@ def test_identity():
     input_data = np.array([20])
     assert np.array_equal(identity(input_data), input_data)
 
+
 def test_relu():
     relu = Relu()
     assert relu(5) == 5
     assert relu(-5) == 0
     assert relu(0) == 0
+
 
 def test_neuron():
     n_in = 3
@@ -21,35 +24,38 @@ def test_neuron():
     output = neuron(x)
     assert isinstance(output, np.float64)  # Ensure output is of correct type
 
+
 def test_solve_AND():
     n_in = 2
     x1 = [0, 0, 1, 1]
     x2 = [0, 1, 0, 1]
-    y =  np.array([0, 0, 0, 1])
+    y = np.array([0, 0, 0, 1])
     clf = NaiveClassifier()
 
     # define 1 possible solution
-    w=np.array([1, 2])
-    b=-2
+    w = np.array([1, 2])
+    b = -2
     neuron1 = Neuron(n_in, non_lin=True, w=w, b=b)
     # define a 2nd possible solution
-    w=np.array([1, 1])
-    b=-1
+    w = np.array([1, 1])
+    b = -1
     neuron2 = Neuron(n_in, non_lin=True, w=w, b=b)
 
     for i, x in enumerate(zip(x1, x2)):
-        assert np.array_equal(clf(neuron1(np.array(x))), clf(neuron2(np.array(x))), y[i])
+        assert np.array_equal(clf(neuron1(np.array(x))),
+                              clf(neuron2(np.array(x))), y[i])
+
 
 def test_solve_OR():
     n_in = 2
     x1 = [0, 0, 1, 1]
     x2 = [0, 1, 0, 1]
-    y =  np.array([0, 1, 1, 1])
+    y = np.array([0, 1, 1, 1])
     clf = NaiveClassifier()
 
     # define 1 possible solution
-    w=np.array([1, 1])
-    b=-0.5
+    w = np.array([1, 1])
+    b = -0.5
     neuron1 = Neuron(n_in, non_lin=True, w=w, b=b)
     for i, x in enumerate(zip(x1, x2)):
         assert np.array_equal(clf(neuron1(np.array(x))), y[i])
