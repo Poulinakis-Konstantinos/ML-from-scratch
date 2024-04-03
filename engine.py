e712bfa9b3efc17ad04d02a32b74bec28e9b8a9e
Poulinakis_Konstantinos <poulinakis.kon@gmail.com>
2024-04-03 09:49:39 +0200

    backpropagation algorithm

--- engine.py
+++ engine.py
@@ -46,3 +46,51 @@ class Tensor:
         return ops[op]
 
 
+def backward(tensor, _upstream_grad=None):
+    assert isinstance(
+        tensor, Tensor), f"Cannot perform backward operation on non-type {Tensor}"
+
+    tensor.grad = Tensor(0) if _upstream_grad is None else _upstream_grad.value
+    up = Tensor(1) if _upstream_grad is None else _upstream_grad
+    assert isinstance(up, Tensor), f"Upstream gradient not of type {Tensor}"
+
+    if len(tensor.prev) == 2:
+        l1, l2 = __calc_grads__(tensor.op, tensor.prev[0], tensor.prev[1])
+        downstream_grad1 = l1 * up
+        downstream_grad2 = l2 * up
+        backward(tensor.prev[0], downstream_grad1)
+        backward(tensor.prev[1], downstream_grad2)
+    elif len(tensor.prev) == 1:
+        l1 = __calc_grad__(tensor.op, tensor.prev[0])
+        downstream_grad1 = l1 * up
+        backward(tensor.prev[0], downstream_grad1)
+
+
+def __calc_grads__(op, _op1, _op2, _precision=0.000001):
+    h = Tensor(_precision)
+    local_grad_1 = (op(_op1 + h, _op2) - op(_op1, _op2)) / h
+    local_grad_2 = (op(_op1, _op2 + h) - op(_op1, _op2)) / h
+    return (local_grad_1, local_grad_2)
+
+
+def __calc_grad__(op, _op1, _precision=0.000001):
+    h = Tensor(_precision)
+    local_grad = (op(_op1 + h) + op(_op1)) / h
+    return local_grad
+
+
+if __name__ == "__main__":
+    er = 0.000001
+    a = Tensor(3)
+    b = Tensor(1)
+    c = Tensor(-2)
+    d = Tensor(2) * b
+    e = a + d
+    L = c*e
+
+    backward(L)
+    assert (a.grad - (-2)) < er, f"Error in gradient calculation of {a}"
+    assert (b.grad - (-4)) < er, f"Error in gradient calculation of {b}"
+    assert (c.grad - (5)) < er, f"Error in gradient calculation of {c}"
+    assert (d.grad - (-2)) < er, f"Error in gradient calculation of {d}"
+    assert (e.grad - (-2)) < er, f"Error in gradient calculation of {e}"
